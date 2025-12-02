96c225a6005c71067829593832ac78ee5d053abf
Plamen Draganov <plamine95@gmail.com>
2025-12-02 08:22:35 +0200

    refactor login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,36 +9,26 @@ import Register from "./components/register/Register.jsx"
 import { useState } from "react"
 import Login from "./components/login/Login.jsx"
 import Edit from "./components/edit/Edit.jsx"
-import request from "./utils/requester.js"
 import UserContext from "./contexts/UserContext.js"
+import useFetch from "./hooks/useFetch.js"
 
 function App() {
     const [user, setUser] = useState(null);
+    const { request } = useFetch();
 
     const registerHandler = async (email, password) => {
         const newUser = { email, password };
 
-        const response = await fetch('http://localhost:3030/users/register', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(newUser),
-        });
+        //register api call
+        const result = await request('/users/register', 'POST', newUser);
 
-        const result = await response.json();
-
+        // login user after register
         setUser(result);
     }
 
-    const loginHandler = (email, password) => {
-
-
-        if (!user) {
-            throw new Error('No such user!');
-        }
-
-        setUser(user);
+    const loginHandler = async (email, password) => {
+        const result = await request('/users/login', 'POST', { email, password })
+        setUser(result);
     }
 
     const logoutHandler = () => {
@@ -64,7 +54,7 @@ function App() {
                 <Route path="/games/:gameId/edit" element={<Edit />} />
                 <Route path="/games/:gameId/details" element={<Details user={user} />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login onLogin={loginHandler} />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/logout" element={<Login onLogout={logoutHandler} />} />
             </Routes>
 
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,41 +1,45 @@
 import { useNavigate } from "react-router";
+import useForm from "../../hooks/useForm.js";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext.js";
 
-export default function Login({
-    onLogin,
-}) {
+export default function Login() {
     const navigate = useNavigate();
+    const {loginHandler} = useContext(UserContext);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+    const submitHandler = async ({ email, password }) => {
 
-        const formData = new FormData(e.target);
-
-        const email = formData.get('email');
-        const password = formData.get('password');
-
-        if(!email || !password) {
+        if (!email || !password) {
             return alert('Email and password are required!');
         }
 
         try {
-            onLogin(email, password);
+            await loginHandler(email, password)
             navigate('/');
         } catch (error) {
             alert(error.message)
         }
     }
 
+    const {
+        register,
+        formAction
+    } = useForm(submitHandler, {
+        email: '',
+        password: ''
+    })
+
     return (
         <section id="login-page">
 
-            <form id="login" onSubmit={submitHandler}>
+            <form id="login" action={formAction}>
                 <div className="container">
                     <h1>Login</h1>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Your Email" />
+                    <input type="email" id="email" {...register('email')} placeholder="Your Email" />
 
                     <label htmlFor="login-pass">Password</label>
-                    <input type="password" id="login-password" name="password" placeholder="Password" />
+                    <input type="password" id="login-password" {...register('password')} placeholder="Password" />
                     <input type="submit" className="btn submit" value="Login" />
                 </div>
             </form>
